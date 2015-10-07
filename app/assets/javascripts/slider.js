56f6d7f84822a558199b54a5248b4bd5096402c7
nazrul <nazrulku07@gmail.com>
2015-10-07 23:57:46 +0600

    Fixed menu and google map issue

--- app/assets/javascripts/slider.js
+++ app/assets/javascripts/slider.js
@@ -58,23 +58,25 @@ jQuery(document).ready(function ($) {
         }
     };
 
-    var jssor_slider1 = new $JssorSlider$("slider1_container", options);
+    if($('#slider1_container').length > 0) {
+        var jssor_slider1 = new $JssorSlider$("slider1_container", options);
 
-    //responsive code begin
-    //you can remove responsive code if you don't want the slider scales while window resizes
-    function ScaleSlider() {
-        var bodyWidth = document.body.clientWidth;
-        if (bodyWidth)
-            jssor_slider1.$ScaleWidth(Math.min(bodyWidth, 1920));
-        else
-            window.setTimeout(ScaleSlider, 30000000);
+        //responsive code begin
+        //you can remove responsive code if you don't want the slider scales while window resizes
+        function ScaleSlider() {
+            var bodyWidth = document.body.clientWidth;
+            if (bodyWidth)
+                jssor_slider1.$ScaleWidth(Math.min(bodyWidth, 1920));
+            else
+                window.setTimeout(ScaleSlider, 30000000);
+        }
+        ScaleSlider();
+
+        $(window).bind("load", ScaleSlider);
+        $(window).bind("resize", ScaleSlider);
+        $(window).bind("orientationchange", ScaleSlider);
+        //responsive code end
     }
-    ScaleSlider();
-
-    $(window).bind("load", ScaleSlider);
-    $(window).bind("resize", ScaleSlider);
-    $(window).bind("orientationchange", ScaleSlider);
-    //responsive code end
 });
 
 //$(document).ready(function ($) {
