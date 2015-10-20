7029465c2983ff1309291ac7d09e63068d299738
nazrul <nazrulku07@gmail.com>
2015-10-20 13:23:42 +0600

    Mail configaration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -98,20 +98,30 @@ Kassi::Application.configure do
 
   mail_delivery_method = (APP_CONFIG.mail_delivery_method.present? ? APP_CONFIG.mail_delivery_method.to_sym : :sendmail)
 
-  config.action_mailer.delivery_method = mail_delivery_method
-  if mail_delivery_method == :postmark
-    config.action_mailer.postmark_settings = { :api_key => APP_CONFIG.postmark_api_key }
-  elsif mail_delivery_method == :smtp
-    ActionMailer::Base.smtp_settings = {
-      :address              => APP_CONFIG.smtp_email_address,
-      :port                 => APP_CONFIG.smtp_email_port,
-      :domain               => APP_CONFIG.smtp_email_domain,
-      :user_name            => APP_CONFIG.smtp_email_user_name,
-      :password             => APP_CONFIG.smtp_email_password,
-      :authentication       => 'plain',
-      :enable_starttls_auto => true
-    }
-  end
+  # config.action_mailer.delivery_method = mail_delivery_method
+  # if mail_delivery_method == :postmark
+  #   config.action_mailer.postmark_settings = { :api_key => APP_CONFIG.postmark_api_key }
+  # elsif mail_delivery_method == :smtp
+  #   ActionMailer::Base.smtp_settings = {
+  #     :address              => APP_CONFIG.smtp_email_address,
+  #     :port                 => APP_CONFIG.smtp_email_port,
+  #     :domain               => APP_CONFIG.smtp_email_domain,
+  #     :user_name            => APP_CONFIG.smtp_email_user_name,
+  #     :password             => APP_CONFIG.smtp_email_password,
+  #     :authentication       => 'plain',
+  #     :enable_starttls_auto => true
+  #   }
+  # end
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      address:              'smtp.gmail.com',
+      port:                 587,
+      domain:               'gmail.com',
+      user_name:            'donot.reply.codejam@gmail.com',
+      password:             'codejam2015',
+      authentication:       'plain',
+      enable_starttls_auto: true  }
 
   # Sendmail is used for some mails (e.g. Newsletter) so configure it even when postmark is the main method
   ActionMailer::Base.sendmail_settings = {
